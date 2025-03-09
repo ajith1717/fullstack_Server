a06410737547d4ed9fc0e3e297c3c7897640fce0
Subramaniam <54570377+ajith1717@users.noreply.github.com>
2025-03-09 23:24:33 +0530

    fix

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 // const { server, io } = require("./websocket/socket");
 const showBanner = require('node-banner');
 
-const port = process.env.PORT || 6005;
+const port = process.env.PORT || 5050;
 
 // Determine the current environment
 // const env = 'production'; // for production
